e2bf7f9fabdd2d6e60bd7a2415eae0244c4032e4
andy-h1 <a.huynh2612@gmail.com>
2020-07-19 22:56:27 +0100

    feat(component): building out page components
    building the basic components for each page

---
+++ src/components/App/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../Header';
+import { HomePage } from '../HomePage';
+import { GamePage } from '../GamePage';
+import { StreamerPage } from '../StreamerPage';
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/game" component={GamePage} />
+      <Route path="/streamers" component={StreamerPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
+
+// Home Page
+// Streamer Page
+// Gaming Page
+// Adding Firebase
+//
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <link to="/">home</link>
+    <link to="/game">games</link>
+    <link to="/streamers">streamers</link>
+  </div>
+);
+
+export default Header;
