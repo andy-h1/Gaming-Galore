a8eef377354c4fbdf762f22c747309d85c1c78b0
andy-h1 <a.huynh2612@gmail.com>
2020-07-21 00:14:47 +0100

    feat(component): add footer component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as S from './styles';
 
 export const Header = () => (
-  <div>
+  <S.Header>
     <Link to="/">home</Link>
     <Link to="/game">games</Link>
     <Link to="/streamers">streamers</Link>
-  </div>
+  </S.Header>
 );
---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  font-family: 'Raleway', sans-serif;
+  text-transform: capitalize;
+  text-decoration: none;
+`;
